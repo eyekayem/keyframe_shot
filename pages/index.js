1616001c6b1004694abaf3a8e9d3d95ddbe43d37
kenny miller <116318994+eyekayem@users.noreply.github.com>
2025-02-11 15:41:50 -0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import CreateIdea from "../components/CreateIdea";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Create a New Idea</h1>
+            <CreateIdea />
+        </div>
+    );
+}
