2e3e7afe5e5b3cc5f6bb1ff7860009b5b0933d76
kenny miller <116318994+eyekayem@users.noreply.github.com>
2025-02-13 19:41:02 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-console.log("Runtime DATABASE_URL:", process.env.DATABASE_URL);
-console.log("Runtime REPLICATE_API_TOKEN:", process.env.REPLICATE_API_TOKEN);
-
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
