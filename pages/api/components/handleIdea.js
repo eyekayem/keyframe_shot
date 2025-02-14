12aeec8c0e050a10f9de6af089a4f45d11733c60
kenny miller <116318994+eyekayem@users.noreply.github.com>
2025-02-13 16:15:56 -0800

    Update handleIdea.js - remove imports

--- pages/api/components/handleIdea.js
+++ pages/api/components/handleIdea.js
@@ -1,7 +1,3 @@
-require('dotenv').config();
-import { v4 as uuidv4 } from "uuid";
-import { neon } from "@neondatabase/serverless";
-
 export async function handleIdea(req) {
     const { user_id, title } = req.body;
     const ideaId = uuidv4();
